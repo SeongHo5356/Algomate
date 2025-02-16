a1a3ade637df5e1330075a4a9ea7a98be9ef7a18
seongho5356 <kjeng7897@gmail.com>
2025-02-17 00:32:51 +0900

    [chore]: initial commit

---
+++ crawling/config/celery_config.py
@@ -0,0 +1,15 @@
+from celery import Celery
+
+celery = Celery(
+    "tasks",
+    broker = "redis://127.0.0.1:6379/0", # Redis를 브로커로 사용
+    backend = "redis://127.0.0.1:6379/0", # 작업 결과 저장
+)
+
+celery.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+    timezone="Asia/Seoul",
+    enable_utc=True,
+)
